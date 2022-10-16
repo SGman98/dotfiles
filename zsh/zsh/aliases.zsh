cd577070f055e1adb33ed791290e80da9ac276c8
SGman98 <bastiangarnica@gmail.com>
2022-10-16 10:13:59 -0500

    Add aliases for docker

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -17,3 +17,8 @@ alias pyenv="python -m venv"
 
 # ---- Pacman ----
 alias pac="sudo pacman"
+
+# ---- Docker ----
+alias dcu="docker compose up"
+alias dcd="docker compose down"
+alias dcr="docker compose down && docker compose up -d --build"
