4d3d272b74ce229db1186589560199ece7c1ec26
SGman98 <bastiangarnica@gmail.com>
2022-10-16 10:11:25 -0500

    Add gitignore plugin for gitignore.io

---
+++ zsh/zsh/plugins/gitignore.plugin.zsh
@@ -0,0 +1,15 @@
+# Plugin from ohmyzsh gitignore
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/gitignore/gitignore.plugin.zsh
+
+function gi() { curl -fLw '\n' https://www.gitignore.io/api/"${(j:,:)@}" }
+
+_gitignoreio_get_command_list() {
+  curl -sfL https://www.gitignore.io/api/list | tr "," "\n"
+}
+
+_gitignoreio () {
+  compset -P '*,'
+  compadd -S '' `_gitignoreio_get_command_list`
+}
+
+compdef _gitignoreio gi
