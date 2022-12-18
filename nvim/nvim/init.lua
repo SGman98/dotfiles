2cea9a72c94143d168d5f1008dc4e148122b4e2c
SGman98 <bastiangarnica@gmail.com>
2022-12-18 17:00:55 -0500

    Add clipboard configuration for wsl in nvim

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,1 +1,2 @@
+require("custom.clipboard")
 require("custom.packer")
---
+++ nvim/nvim/lua/custom/clipboard.lua
@@ -0,0 +1,16 @@
+in_wsl = os.getenv('WSL_DISTRO_NAME') ~= nil
+
+if in_wsl then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = true,
+    }
+end
