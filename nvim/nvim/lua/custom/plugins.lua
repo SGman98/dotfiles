95912ba9a4d65561babe36a6ace1b538c98538e6
SGman98 <bastiangarnica@gmail.com>
2022-12-18 19:05:29 -0500

    Add git related plugins to nvim

--- nvim/nvim/lua/custom/plugins.lua
+++ nvim/nvim/lua/custom/plugins.lua
@@ -10,4 +10,9 @@ return function(use)
 
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+
+    -- Git related plugins
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rhubarb'
+    use 'lewis6991/gitsigns.nvim'
 end
