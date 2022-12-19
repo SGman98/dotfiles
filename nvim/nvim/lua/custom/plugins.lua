a36b0b60690b1dbc872230d2d432d6daf330a622
SGman98 <bastiangarnica@gmail.com>
2022-12-18 23:05:03 -0500

    Add undotree plugin to nvim

--- nvim/nvim/lua/custom/plugins.lua
+++ nvim/nvim/lua/custom/plugins.lua
@@ -4,6 +4,7 @@ return function(use)
     use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
     use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
     use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+    use 'mbbill/undotree'
 
     -- Fuzzy Finder (files, lsp, etc)
     use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
