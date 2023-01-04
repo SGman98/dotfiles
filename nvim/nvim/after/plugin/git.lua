db6fa069e550858610ad8ce5d2d47ab21e83b44f
SGman98 <bastiangarnica@gmail.com>
2023-01-03 22:24:13 -0500

    Update project files finder in telescope

--- nvim/nvim/after/plugin/git.lua
+++ nvim/nvim/after/plugin/git.lua
@@ -3,7 +3,6 @@ local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>gg", ":G | only<CR>", { desc = "Git fugitive" })
 vim.keymap.set("n", "<leader>gb", telescope.git_branches, { desc = "Git commits" })
 vim.keymap.set("n", "<leader>gc", telescope.git_commits, { desc = "Git commits" })
-vim.keymap.set("n", "<leader>gf", telescope.git_files, { desc = "Git files" })
 vim.keymap.set("n", "<leader>gt", telescope.git_stash, { desc = "Git stash" })
 
 require("gitsigns").setup({
--- nvim/nvim/after/plugin/telescope.lua
+++ nvim/nvim/after/plugin/telescope.lua
@@ -3,13 +3,21 @@ pcall(require("telescope").load_extension, "fzf")
 
 local telescope = require("telescope.builtin")
 
+local function project_files()
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    if vim.v.shell_error == 0 then
+        require("telescope.builtin").git_files()
+    else
+        require("telescope.builtin").find_files()
+    end
+end
+
 vim.keymap.set("n", "<leader>sb", telescope.buffers, { desc = "Search buffers" })
 vim.keymap.set("n", "<leader>sc", telescope.grep_string, { desc = "Search current word" })
 vim.keymap.set("n", "<leader>sd", telescope.diagnostics, { desc = "Search diagnostics" })
 vim.keymap.set("n", "<leader>sd", telescope.diagnostics, { desc = "Search diagnostics" })
-vim.keymap.set("n", "<leader>sf", telescope.find_files, { desc = "Search files" })
+vim.keymap.set("n", "<leader>sf", project_files, { desc = "Search project files" })
 vim.keymap.set("n", "<leader>sh", telescope.help_tags, { desc = "Search help" })
 vim.keymap.set("n", "<leader>sk", telescope.keymaps, { desc = "Search keymaps" })
 vim.keymap.set("n", "<leader>so", telescope.oldfiles, { desc = "Search oldfiles" })
 vim.keymap.set("n", "<leader>sw", telescope.live_grep, { desc = "Search by grep" })
-vim.keymap.set("n", "<leader>sg", telescope.git_files, { desc = "Search Git Files" })
