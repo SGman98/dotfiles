0c40fceaf1de2203e572bfd7f86a61a1811b8575
SGman98 <bastiangarnica@gmail.com>
2023-01-12 20:15:32 -0500

    Update neovim config files with new manage script

---
+++ nvim/manage.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# shellcheck source=/dev/null
+source "$HOME/.dotfiles/functions.sh"
+
+PROMPT="::neovim-BOOTSTRAP::"
+
+case "$1" in
+    setup)
+        confirm "Do you want to setup neovim?" "Y" || abort "Aborting..."
+        install_package "neovim" || abort "Failed to install neovim"
+
+        check_path_link "$HOME/.config/nvim/init.lua" "$HOME/.dotfiles/nvim/init.lua"
+        check_path_link "$HOME/.config/nvim/lua" "$HOME/.dotfiles/nvim/lua"
+        check_path_link "$HOME/.config/nvim/after" "$HOME/.dotfiles/nvim/after"
+        ;;
+    remove)
+        if [[ -d $HOME/.config/nvim/ ]] ; then
+            confirm "Do you want to remove $HOME/.config/nvim/?" "Y" || abort "Aborting..."
+            rm -rf "$HOME/.config/nvim/" || abort "Failed to remove $HOME/.config/nvim/ folder"
+            success "Removed correctly"
+        fi
+        ;;
+    update) ;; # TODO
+    *) abort "Usage: $0 {setup|remove|update}" ;;
+esac
