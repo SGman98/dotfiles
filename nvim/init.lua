9106f37927fbc1f3a53fb3505fbc0d30e16e3452
SGman98 <bastiangarnica@gmail.com>
2023-01-15 18:35:56 -0500

    Migrate neovim plugin manager packer -> lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("lazy").setup("plugins")
+
 require("custom.clipboard")
-require("custom.packer")
 require("custom.set")
 require("custom.remap")
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,42 +1,21 @@
-return function(use)
+return {
     -- Extras
-    use("mbbill/undotree")
-    use("navarasu/onedark.nvim")
-    use("nvim-tree/nvim-tree.lua")
-    use("theprimeagen/harpoon")
-    use("tpope/vim-sleuth")
-    use("johmsalas/text-case.nvim")
-    use("christoomey/vim-tmux-navigator")
-    use("tpope/vim-obsession")
+    "mbbill/undotree",
+    "navarasu/onedark.nvim",
+    "nvim-tree/nvim-tree.lua",
+    "theprimeagen/harpoon",
+    "tpope/vim-sleuth",
+    "johmsalas/text-case.nvim",
+    "christoomey/vim-tmux-navigator",
+    "tpope/vim-obsession",
 
-    use({
-        "folke/zen-mode.nvim",
-        config = function()
-            require("zen-mode").setup({})
-        end,
-    })
-    use({
-        "kylechui/nvim-surround",
-        tag = "*",
-        config = function()
-            require("nvim-surround").setup({})
-        end,
-    })
-    use({
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup({})
-        end,
-    })
-    use({
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup({})
-        end,
-    })
-    use({
+    { "folke/zen-mode.nvim", config = {} },
+    { "kylechui/nvim-surround", config = {}, version = "*" },
+    { "numToStr/Comment.nvim", config = {} },
+    { "nvim-lualine/lualine.nvim", config = {} },
+    {
         "unblevable/quick-scope",
-        setup = function()
+        init = function()
             vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
             vim.g.rainbow_active = 1
             vim.api.nvim_create_autocmd("ColorScheme", {
@@ -48,24 +27,24 @@ return function(use)
                 command = "highlight QuickScopeSecondary guifg=#00ffff gui=underline ctermfg=81 cterm=underline",
             })
         end,
-    })
+    },
 
     -- Fuzzy Finder (files, lsp, etc)
-    use("stevearc/dressing.nvim")
-    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } })
+    "stevearc/dressing.nvim",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 })
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
 
     -- Git related plugins
-    use("tpope/vim-fugitive")
-    use("lewis6991/gitsigns.nvim")
-    use("kdheepak/lazygit.nvim")
+    "tpope/vim-fugitive",
+    "lewis6991/gitsigns.nvim",
+    "kdheepak/lazygit.nvim",
 
     -- Language servers and related
-    use({
+    {
         "VonHeikemen/lsp-zero.nvim",
-        requires = {
+        dependencies = {
             -- LSP Support
             "neovim/nvim-lspconfig",
             "williamboman/mason.nvim",
@@ -87,20 +66,18 @@ return function(use)
             "L3MON4D3/LuaSnip",
             "rafamadriz/friendly-snippets",
         },
-    })
+    },
 
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("mfussenegger/nvim-dap")
+    "jose-elias-alvarez/null-ls.nvim",
+    "mfussenegger/nvim-dap",
 
-    use({ -- Highlight, edit, and navigate code
+    { -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
-        run = function()
+        build = function()
             pcall(require("nvim-treesitter.install").update({ with_sync = true }))
         end,
-    })
-
-    use({ -- Additional text objects via treesitter
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter",
-    })
-end
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+    },
+}
