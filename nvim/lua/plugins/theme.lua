5efc7b076b125a62e8fc1a905d7b73424319e329
SGman98 <bastiangarnica@gmail.com>
2023-01-23 22:07:21 -0500

    Update neovim theme config with transparent bg

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,6 +2,10 @@ return {
     "navarasu/onedark.nvim",
     lazy = false,
     config = function()
-        vim.cmd([[colorscheme onedark]])
+        require("onedark").setup({
+            style = "darker",
+            transparent = true,
+        })
+        require("onedark").load()
     end,
 }
