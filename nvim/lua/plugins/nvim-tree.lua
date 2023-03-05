cbaff7998b3ea7978e046d3a84234860aa37c545
SGman98 <bastiangarnica@gmail.com>
2023-03-04 22:55:38 -0500

    Add dev icons to nvimtree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,16 +1,19 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    keys = {
-        { "<leader>e", vim.cmd.NvimTreeToggle },
-        { "<leader>o", vim.cmd.NvimTreeFocus },
-    },
-    opts = {
-        update_focused_file = {
-            enable = true,
-            update_cwd = true,
+    { 
+        "nvim-tree/nvim-tree.lua",
+        keys = {
+            { "<leader>e", vim.cmd.NvimTreeToggle },
+            { "<leader>o", vim.cmd.NvimTreeFocus },
         },
-        view = {
-            side = "right",
+        opts = {
+            update_focused_file = {
+                enable = true,
+                update_cwd = true,
+            },
+            view = {
+                side = "right",
+            },
         },
     },
+    { "kyazdani42/nvim-web-devicons" },
 }
