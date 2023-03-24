f416f569955a59214b24f2a0e0233ee0dbf94ac5
SGman98 <bastiangarnica@gmail.com>
2023-03-24 11:11:02 -0500

    Change quick-scope plugin for flit/leap

--- nvim/lua/plugins/quick-scope.lua
+++ nvim/lua/plugins/quick-scope.lua
@@ -1,15 +1,16 @@
 return {
-    "unblevable/quick-scope",
-    init = function()
-        vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-        vim.g.rainbow_active = 1
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            pattern = { "*" },
-            command = "highlight QuickScopePrimary guifg=#00ff00 gui=underline ctermfg=155 cterm=underline",
-        })
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            pattern = { "*" },
-            command = "highlight QuickScopeSecondary guifg=#00ffff gui=underline ctermfg=81 cterm=underline",
-        })
-    end,
+    {
+        "ggandor/flit.nvim",
+        dependencies = {
+            "ggandor/leap.nvim",
+        },
+        keys = function()
+            local ret = {}
+            for _, key in ipairs({ "f", "F", "t", "T" }) do
+                ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+            end
+            return ret
+        end,
+        opts = { labeled_modes = "nx" },
+    },
 }
