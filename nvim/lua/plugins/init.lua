1aa87d4b966ff93810410a3802083042c6c8974d
SGman98 <bastiangarnica@gmail.com>
2023-04-16 22:38:06 -0500

    Fix nvim-colorizer again...

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -9,12 +9,7 @@ return {
         "NvChad/nvim-colorizer.lua",
         opts = {
             filetypes = {
-                "*",
-                "!fugitive", -- it was causing issues in status file
-            },
-            user_default_options = {
-                css = true,
-                sass = { enable = true },
+                scss = { css = true, sass = { enable = true } },
             },
         },
     },
