902b998983e6c7ba308bda8a68a3fcb0a586ef1c
SGman98 <bastiangarnica@gmail.com>
2023-04-16 23:10:25 -0500

    Fix git manage script

--- git/manage.sh
+++ git/manage.sh
@@ -9,32 +9,26 @@ setup)
 	confirm "Do you want to setup git?" "Y" || abort "Aborting..."
 	install_package "git" || abort "Failed to install git"
 
-	# Try to get GIT_USERNAME and GIT_EMAIL from git config
-	GIT_USERNAME=$(git config --global user.name)
-	GIT_EMAIL=$(git config --global user.email)
-	GIT_SIGNINGKEY=$(git config --global user.signingkey)
-	GPG_SIGNINGKEY=$(gpg --list-secret-keys --keyid-format LONG | grep sec | cut -d"/" -f2 | cut -d" " -f1)
-
 	check_path_link "$HOME/.gitconfig"
 	check_path_link "$HOME/.config/git/config" "$HOME/.dotfiles/git/config"
 	check_path_link "$HOME/.config/git/gitmessage.txt" "$HOME/.dotfiles/git/gitmessage.txt"
 
-	ask "Enter your git username$([[ -n $GIT_USERNAME ]] && echo " [$GIT_USERNAME]"): " && GIT_USERNAME=${RES:-$GIT_USERNAME}
-	ask "Enter your git email$([[ -n $GIT_EMAIL ]] && echo " [$GIT_EMAIL]"): " && GIT_EMAIL=${RES:-$GIT_EMAIL}
-
-	confirm "Your git username is $GIT_USERNAME and your git email is $GIT_EMAIL. Is it correct?" "Y" || abort "Aborting..."
-
-	[[ -n $GIT_USERNAME ]] && echo -e "[user]\n\tname = $GIT_USERNAME" | tee -a "$HOME/.config/git/personal.gitconfig" &>/dev/null
-	[[ -n $GIT_EMAIL ]] && echo -e "[user]\n\temail = $GIT_EMAIL" | tee -a "$HOME/.config/git/personal.gitconfig" &>/dev/null
-
-	if [[ -n $GIT_SIGNINGKEY ]]; then
-		confirm "Do you want to keep your signingkey in your git config [$GIT_SIGNINGKEY]?" "Y" && SIGNINGKEY="$GIT_SIGNINGKEY"
-	fi
-	if [[ -n $GPG_SIGNINGKEY ]] && [[ -z $SIGNINGKEY ]]; then
-		confirm "Do you want to use your signingkey from gpg [$GPG_SIGNINGKEY]?" "Y" && SIGNINGKEY="$GPG_SIGNINGKEY"
+	if [[ -z $(git config --global --includes user.name) ]]; then
+		ask "Enter your git username: " &&
+			echo -e "[user]\n\tname = $RES" | tee -a "$HOME/.config/git/personal.gitconfig" &>/dev/null
 	fi
 
-	[[ -n $SIGNINGKEY ]] && echo -e "[user]\n\tsigningkey = $SIGNINGKEY" | tee -a "$HOME/.config/git/personal.gitconfig" &>/dev/null
+	if [[ -z $(git config --global --includes user.email) ]]; then
+		ask "Enter your git email: " &&
+			echo -e "[user]\n\temail = $RES" | tee -a "$HOME/.config/git/personal.gitconfig" &>/dev/null
+	fi
+
+	GIT_SIGNINGKEY=$(git config --global --includes user.signingkey)
+	GPG_SIGNINGKEY=$(gpg --list-secret-keys --keyid-format LONG | grep sec | cut -d"/" -f2 | cut -d" " -f1)
+	if [[ -z $GIT_SIGNINGKEY ]] && [[ -n $GPG_SIGNINGKEY ]]; then
+		confirm "Do you want to use your signingkey from gpg [$GPG_SIGNINGKEY]?" "Y" &&
+			echo -e "[user]\n\tsigningkey = $GPG_SIGNINGKEY" | tee -a "$HOME/.config/git/personal.gitconfig" &>/dev/null
+	fi
 
 	success "Setup correctly"
 	;;
