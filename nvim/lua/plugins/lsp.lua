73d7c6f4a90a41da88d4ec7fbb85bb24a1b87511
SGman98 <bastiangarnica@gmail.com>
2023-06-06 17:29:28 -0500

    Add trouble plugin for neovim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -60,4 +60,20 @@ return {
             })
         end,
     },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "kyazdani42/nvim-web-devicons" },
+        opts = {
+            auto_close = true,
+            use_diagnostic_signs = true,
+        },
+        keys = {
+            { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Trouble toggle" },
+            { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Trouble workspace diagnostics" },
+            { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Trouble document diagnostics" },
+            { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Trouble loclist" },
+            { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Trouble quickfix" },
+            { "<leader>gr", "<cmd>TroubleToggle lsp_references<cr>", desc = "Trouble references" },
+        },
+    },
 }
