416dbd191c76eca9cd9584c3a6a37fb9152236ef
SGman98 <bastiangarnica@gmail.com>
2023-06-06 14:52:11 -0500

    Update `packages.sh`

--- packages.sh
+++ packages.sh
@@ -9,28 +9,23 @@ pkgs=(
 	# base-devel  # Base development tools # Needs fix fakeroot with --no-confirm
 	wget # Download files
 
-	# Controlled with dotfiles
-	git      # Version control
-	fish     # Shell
-	zsh      # Shell
-	starship # Shell prompt
-	neovim   # Text editor
-	tmux     # Terminal multiplexer
-
 	# Security
 	openssh # SSH server
 	gnupg   # GPG
 
 	# CLI Tools
-	bat       # Cat clone with syntax highlighting
-	exa       # ls clone
-	fd        # Find files
-	fzf       # Fuzzy finder
-	ripgrep   # Search files
-	jq        # JSON parser
-	htop      # Process viewer
-	thefuck   # Correct mistyped commands
-	git-delta # Git diff viewer
+	bat         # Cat clone with syntax highlighting
+	bat-extras  # Bat extensions
+	exa         # ls clone
+	fd          # Find files
+	fzf         # Fuzzy finder
+	ripgrep     # Search files
+	jq          # JSON parser
+	htop        # Process viewer
+	thefuck     # Correct mistyped commands
+	git-delta   # Git diff viewer
+	gitflow-avh # Git flow command tool
+   	github-cli  # Git hub cli
 
 	# Fun
 	cowsay      # Cow that says things
