85e08f13dedd6580df99530a629066f702793494
SGman98 <bastiangarnica@gmail.com>
2023-06-26 16:45:17 -0500

    Update notifications config for neovim

--- nvim/lua/plugins/notifications.lua
+++ nvim/lua/plugins/notifications.lua
@@ -1,41 +1,33 @@
 return {
     {
-        "rcarriga/nvim-notify",
-        dependencies = "nvim-telescope/telescope.nvim",
-        config = function()
-            require("telescope").load_extension("notify")
-            require("notify").setup({
-                background_colour = "#000000",
-                top_down = false,
-            })
-        end,
-    },
-    {
         "folke/noice.nvim",
+        -- event = "VeryLazy", -- Need fix, message when opening nvim
         dependencies = {
             "MunifTanjim/nui.nvim",
-            "nvim-telescope/telescope.nvim",
-            "rcarriga/nvim-notify",
+            {
+                "rcarriga/nvim-notify",
+                opts = {
+                    background_colour = "#000000",
+                    top_down = false,
+                },
+            },
         },
-        config = function()
-            require("telescope").load_extension("noice")
-            require("noice").setup({
-                cmdline = {
-                    view = "cmdline",
+        opts = {
+            cmdline = {
+                view = "cmdline",
+            },
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
                 },
-                lsp = {
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true,
-                    },
-                },
-                presets = {
-                    bottom_search = true,
-                    command_palette = true,
-                    long_message_to_split = true,
-                },
-            })
-        end,
+            },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+            },
+        },
     },
 }
