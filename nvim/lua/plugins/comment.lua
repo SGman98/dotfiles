7737b3cfe188bbc0c7f4c5c0a3df1d1a2a6ba28f
SGman98 <bastiangarnica@gmail.com>
2023-06-26 20:07:47 -0500

    Update comment neovim plugin to be lazy loaded

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,5 +1,6 @@
 return {
     "numToStr/Comment.nvim",
+    event = "VeryLazy",
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     config = function()
         require("Comment").setup({
