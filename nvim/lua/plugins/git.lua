b15361d657de248fc8e35c322f52ec3771128bb6
SGman98 <bastiangarnica@gmail.com>
2023-06-26 20:09:00 -0500

    Fix gitsign keymaps for visual mode and fugitive

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,7 +3,7 @@ return {
     {
         "tpope/vim-fugitive",
         keys = {
-            { "<leader>gg", "<cmd>G<cr>", desc = "Git fugitive" },
+            { "<leader>gg", "<cmd>rightbelow vertical G<cr>", desc = "Git fugitive" },
         },
         cmd = "G",
     },
@@ -24,8 +24,10 @@ return {
                 map("n", "[h", gs.prev_hunk)
 
                 -- Actions
-                map({ "n", "v" }, "<leader>hs", gs.stage_hunk)
-                map({ "n", "v" }, "<leader>hr", gs.reset_hunk)
+                map("n", "<leader>hs", gs.stage_hunk)
+                map("n", "<leader>hr", gs.reset_hunk)
+                map("v", "<leader>hs", function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                map("v", "<leader>hr", function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
                 map("n", "<leader>hu", gs.undo_stage_hunk)
                 map("n", "<leader>hp", gs.preview_hunk)
                 map("n", "<leader>hS", gs.stage_buffer)
