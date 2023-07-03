567ab1fc37477181b561c7632055e78a56774034
SGman98 <bastiangarnica@gmail.com>
2023-07-03 12:26:01 -0500

    Remove unnecessary config for rust tools now use opts

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -1,34 +1,32 @@
 return {
     {
         "simrat39/rust-tools.nvim",
-        config = function()
-            require("rust-tools").setup({
-                tools = {
-                    inlay_hints = {
-                        only_current_line = true,
-                    },
-                    hover_actions = {
-                        auto_focus = true,
-                    },
-                    runnables = {
-                        use_telescope = true,
-                    },
+        opts = {
+            tools = {
+                inlay_hints = {
+                    only_current_line = true,
                 },
-                server = {
-                    settings = {
-                        ["rust-analyzer"] = {
-                            lens = {
-                                enable = true,
-                            },
-                            checkOnSave = {
-                                enable = true,
-                                command = "clippy",
-                            },
+                hover_actions = {
+                    auto_focus = true,
+                },
+                runnables = {
+                    use_telescope = true,
+                },
+            },
+            server = {
+                settings = {
+                    ["rust-analyzer"] = {
+                        lens = {
+                            enable = true,
+                        },
+                        checkOnSave = {
+                            enable = true,
+                            command = "clippy",
                         },
                     },
                 },
-            })
-        end,
+            },
+        },
         ft = { "rust", "rs" },
     },
     {
