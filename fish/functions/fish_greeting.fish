6a85587d2d5d0b498b81cedda29e56f96a92606d
SGman98 <bastiangarnica@gmail.com>
2023-09-03 23:57:27 -0500

    Remove greeting from fish

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
     if command -v fortune &>/dev/null; and command -v cowsay &>/dev/null; and command -v lolcat &>/dev/null
-        fortune -sa | cowsay -f(cowsay -l | tail -n +2 | tr ' ' '\n' | sort -R | head -1) -W(math (stty size | cut -d" " -f2) - 3) | lolcat -F 0.01
+        # fortune -sa | cowsay -f(cowsay -l | tail -n +2 | tr ' ' '\n' | sort -R | head -1) -W(math (stty size | cut -d" " -f2) - 3) | lolcat -F 0.01
     end
 end
