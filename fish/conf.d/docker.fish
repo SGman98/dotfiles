990c3f62a7439a184fee29c497f53d5282335ec3
SGman98 <bastiangarnica@gmail.com>
2023-09-03 23:58:46 -0500

    Add env variables only when needed

--- fish/conf.d/docker.fish
+++ fish/conf.d/docker.fish
@@ -1,7 +1,7 @@
-set -gx DOCKER_CONFIG $XDG_CONFIG_HOME/docker
-set -gx MACHINE_STORAGE_PATH $XDG_DATA_HOME/docker-machine
-
 if command -v docker &>/dev/null
+    set -gx DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+    set -gx MACHINE_STORAGE_PATH $XDG_DATA_HOME/docker-machine
+
     abbr -a dk docker
     abbr -a dkcu docker compose up
     abbr -a dkcd docker compose down
--- fish/conf.d/rtx.fish
+++ fish/conf.d/rtx.fish
@@ -1,4 +1,7 @@
 if command -v rtx &>/dev/null
+    set -gx ASDF_CONFIG_FILE $XDG_CONFIG_HOME/asdf/asdfrc
+    set -gx ASDF_DATA_DIR $XDG_DATA_HOME/asdf
+
     rtx activate fish | source
     rtx completions fish | source
 end
