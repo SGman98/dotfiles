26919039749bd5e190f211f79adc353555fdcd22
SGman98 <bastiangarnica@gmail.com>
2023-10-10 22:01:15 -0500

    Add live-server to packages

--- packages.sh
+++ packages.sh
@@ -17,20 +17,21 @@ pkgs=(
 	rtx
 
 	# CLI Tools
-	bat         # Cat clone with syntax highlighting
-	bat-extras  # Bat extensions
-	exa         # ls clone
-	fd          # Find files
-	fzf         # Fuzzy finder
-	git-delta   # Git diff viewer
-	gitflow-avh # Git flow command tool
-	github-cli  # Git hub cli
-	htop        # Process viewer
-	hurl-bin    # HTTP requests and test
-	jq          # JSON parser
-	ripgrep     # Search files
-	thefuck     # Correct mistyped commands
-	tldr        # Simple help for tools
+	bat                # Cat clone with syntax highlighting
+	bat-extras         # Bat extensions
+	exa                # ls clone
+	fd                 # Find files
+	fzf                # Fuzzy finder
+	git-delta          # Git diff viewer
+	gitflow-avh        # Git flow command tool
+	github-cli         # Git hub cli
+	htop               # Process viewer
+	hurl-bin           # HTTP requests and test
+	jq                 # JSON parser
+	ripgrep            # Search files
+	thefuck            # Correct mistyped commands
+	tldr               # Simple help for tools
+	nodejs-live-server # Live server
 
 	# Fun
 	cowsay      # Cow that says things
