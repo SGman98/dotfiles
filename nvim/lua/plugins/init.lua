901a5b7d976480362710e547a811fdd18de8cdb5
SGman98 <bastiangarnica@gmail.com>
2023-10-13 21:27:58 -0500

    Disable `indent-blankline` scope

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -33,7 +33,9 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         event = { "BufReadPre", "BufNewFile" },
         main = "ibl",
-        opts = {},
+        opts = {
+            scope = { enabled = false },
+        },
     },
     {
         "kristijanhusak/vim-dadbod-ui",
