62674b54abd362fadd3b181c1ff24069008d094b
SGman98 <bastiangarnica@gmail.com>
2023-10-13 21:38:25 -0500

    Add nvim tmux compatible movement

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -54,4 +54,28 @@ return {
             vim.g.db_ui_use_nerd_fonts = 1
         end,
     },
+    {
+        "aserowy/tmux.nvim",
+        config = function()
+            local tmux = require("tmux")
+            tmux.setup({
+                navigation = {
+                    cycle_navigation = false,
+                    enable_default_keybindings = false,
+                },
+                resize = {
+                    enable_default_keybindings = false,
+                },
+            })
+
+            vim.keymap.set("n", "<C-Left>", tmux.move_left)
+            vim.keymap.set("n", "<C-Right>", tmux.move_right)
+            vim.keymap.set("n", "<C-Up>", tmux.move_top)
+            vim.keymap.set("n", "<C-Down>", tmux.move_bottom)
+            vim.keymap.set("n", "<S-Left>", tmux.resize_left)
+            vim.keymap.set("n", "<S-Right>", tmux.resize_right)
+            vim.keymap.set("n", "<S-Up>", tmux.resize_top)
+            vim.keymap.set("n", "<S-Down>", tmux.resize_bottom)
+        end,
+    },
 }
