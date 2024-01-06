0e8ad713c196b1cce78568209afe4d2013c2e608
SGman98 <bastiangarnica@gmail.com>
2024-01-06 16:40:34 -0500

    Update icons for nerdfonts

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -105,7 +105,7 @@ return {
             lsp.setup()
 
             -- Signs
-            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticSign" .. type
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
