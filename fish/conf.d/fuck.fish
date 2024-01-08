0bf90843c20b0f1c0f6061ccc756569c5ae1fe0c
SGman98 <bastiangarnica@gmail.com>
2024-01-07 22:15:26 -0500

    Fix thefuck package error when dir doesn't exist

--- fish/conf.d/fuck.fish
+++ fish/conf.d/fuck.fish
@@ -1,6 +1,8 @@
 if command -v fuck &>/dev/null
     set -l line "excluded_search_path_prefixes = ['/mnt/']"
+    if not test -d $XDG_CONFIG_HOME/thefuck
+        mkdir $XDG_CONFIG_HOME/thefuck
+    end
     if not grep -qF $line $XDG_CONFIG_HOME/thefuck/settings.py
     echo $line | tee -a $XDG_CONFIG_HOME/thefuck/settings.py
-    end
 end
