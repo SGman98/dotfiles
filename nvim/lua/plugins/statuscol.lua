74cbc6c626c0390b8b4fd909c01991cae1983713
SGman98 <bastiangarnica@gmail.com>
2024-01-11 22:41:40 -0500

    Fix status col adding empty column for folds

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -6,24 +6,13 @@ return {
             require("statuscol").setup({
                 relculright = true,
                 segments = {
-                    { text = { builtin.foldfunc }, maxwidth = 1, click = "v:lua.ScFa" },
-                    {
-                        sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
-                        click = "v:lua.ScSa",
-                    },
-                    {
-                        sign = { name = { "Dap*" }, maxwidth = 1, auto = true },
-                        click = "v:lua.ScSa",
-                    },
-                    {
-                        sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true },
-                        click = "v:lua.ScSa",
-                    },
+                    { text = { builtin.foldfunc }, auto = true, click = "v:lua.ScFa" },
+                    { text = { " " }, hl = "FoldColumn" }, -- Empty column
+                    { sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true }, click = "v:lua.ScSa" },
+                    { sign = { name = { "Dap*" }, maxwidth = 1, auto = true }, click = "v:lua.ScSa" },
+                    { sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true }, click = "v:lua.ScSa" },
                     { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-                    {
-                        sign = { namespace = { "gitsign*" }, maxwidth = 1, colwidth = 1 },
-                        click = "v:lua.ScSa",
-                    },
+                    { sign = { namespace = { "gitsign*" }, maxwidth = 1, colwidth = 1 }, click = "v:lua.ScSa" },
                 },
             })
         end,
