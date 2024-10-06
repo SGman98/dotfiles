2551eb0036b75c384b73ffea33a700c505da0843
SGman98 <bastiangarnica@gmail.com>
2024-10-05 22:56:28 -0500

    feat: ⬆ add nvim-nio since is needed for nvim-dap-ui

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -16,6 +16,7 @@ return {
         "mfussenegger/nvim-dap",
         dependencies = {
             { "rcarriga/nvim-dap-ui" },
+            { "nvim-neotest/nvim-nio" },
             { "jay-babu/mason-nvim-dap.nvim" },
             { "williamboman/mason.nvim", build = ":MasonUpdate" },
         },
