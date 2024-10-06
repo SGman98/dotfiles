f01ab826c1f839f3ad40d58d19587c9bbc1c3abc
SGman98 <bastiangarnica@gmail.com>
2024-10-05 21:48:15 -0500

    feat: 🔥 stop using lsp-zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,18 +1,27 @@
 local myFunc = require("config.functions")
 
-return {
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        lazy = true,
-        config = false,
-        init = function()
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
-    },
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP Actions",
+    callback = function(event)
+        local function map(mode, l, r, desc)
+            local opts = { buffer = event.buf, desc = desc }
+            vim.keymap.set(mode, l, r, opts)
+        end
+        -- Diagnostics
+        map("n", "<leader>dv", vim.diagnostic.open_float, "View Diagnostics")
+        map("n", "[d", vim.diagnostic.goto_prev)
+        map("n", "]d", vim.diagnostic.goto_next)
+        -- LSP
+        map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+        map("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
+        map("n", "<leader>lh", vim.lsp.buf.hover, "Hover")
+        map("i", "<C-h>", vim.lsp.buf.signature_help)
+        -- Formatting
+        map({ "n", "v" }, "<leader>ff", myFunc.format, "Format")
+    end,
+})
 
-    -- Autocompletion
+return {
     {
         "hrsh7th/nvim-cmp",
         event = { "InsertEnter" },
@@ -47,17 +56,12 @@ return {
             },
         },
         config = function()
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_cmp()
-
             local cmp = require("cmp")
-            local cmp_action = lsp_zero.cmp_action()
-            local cmp_format = lsp_zero.cmp_format()
+            local ls = require("luasnip")
 
             require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
-                formatting = cmp_format,
                 sources = {
                     { name = "nvim_lsp" },
                     { name = "path" },
@@ -66,9 +70,12 @@ return {
                     { name = "buffer", keyword_length = 5 },
                     { name = "copilot" },
                 },
+                snippet = {
+                    expand = function(args) require("luasnip").lsp_expand(args.body) end,
+                },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-                    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+                    ["<C-f>"] = function() ls.jump(1) end,
+                    ["<C-b>"] = function() ls.jump(-1) end,
                 }),
                 completion = {
                     completeopt = "menu,menuone,noinsert",
@@ -89,34 +96,36 @@ return {
         },
 
         config = function()
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_lspconfig()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-            lsp_zero.on_attach(function(_, bufnr)
-                lsp_zero.default_keymaps({ buffer = bufnr })
-                local function map(mode, l, r, desc)
-                    local opts = { desc = desc, buffer = bufnr }
-                    vim.keymap.set(mode, l, r, opts)
-                end
-                map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-                map("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
-                map("n", "<leader>lh", vim.lsp.buf.hover, "Hover")
-                map("i", "<C-h>", vim.lsp.buf.signature_help)
+            local lsp_capabilities = cmp_nvim_lsp.default_capabilities()
 
-                -- Diagnostics
-                map("n", "<leader>dv", vim.diagnostic.open_float, "View Diagnostics")
-                map("n", "[d", vim.diagnostic.goto_prev)
-                map("n", "]d", vim.diagnostic.goto_next)
+            local default_setup = function(server)
+                require("lspconfig")[server].setup({
+                    capabilities = lsp_capabilities,
+                })
+            end
 
-                -- Formatting
-                map({ "n", "v" }, "<leader>ff", myFunc.format, "Format")
-            end)
-
-            require("mason").setup()
+            require("mason").setup({})
             require("mason-lspconfig").setup({
+                ensure_installed = {},
                 handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function() require("lspconfig").lua_ls.setup(lsp_zero.nvim_lua_ls()) end,
+                    default_setup,
+                    lua_ls = function()
+                        require("lspconfig").lua_ls.setup({
+                            settings = {
+                                Lua = {
+                                    runtime = { version = "LuaJIT" },
+                                    diagnostics = { globals = { "vim" } },
+                                    workspace = {
+                                        library = {
+                                            vim.env.VIMRUNTIME,
+                                        },
+                                    },
+                                },
+                            },
+                        })
+                    end,
                     eslint = function()
                         require("lspconfig").eslint.setup({
                             on_attach = function(_, bufnr)
