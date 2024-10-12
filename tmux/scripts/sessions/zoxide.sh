a0f60f6acdc7987cba7587c546d7a1ac8594196d
SGman98 <bastiangarnica@gmail.com>
2024-10-11 21:39:50 -0500

    feat: ✨ update file permissions for scripts files

--- tmux/scripts/sessions/zoxide.sh
+++ tmux/scripts/sessions/zoxide.sh
@@ -35,7 +35,7 @@ if command -v tmux &>/dev/null; then
 	if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
 		tmux new-session -d -s "$SESSION_NAME"
 		if [ -f "$project/.tmux.sh" ]; then
-			tmux send-keys -t "$SESSION_NAME":0.0 ". $project/.tmux.sh" C-m
+			tmux send-keys -t "$SESSION_NAME":0.0 "$project/.tmux.sh" C-m
 		fi
 	fi
 
