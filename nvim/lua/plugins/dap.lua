7377a55cbe006f238821c9308b871d0ac95ee649
SGman98 <bastiangarnica@gmail.com>
2024-11-19 15:24:33 -0500

    refactor(nvim): ♻️ update code structure for dap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -41,23 +41,28 @@ return {
             })
 
             local dap, dapui = require("dap"), require("dapui")
-            dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
-            dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-            dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+            dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+            dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+            dap.listeners.before.event_exited["dapui_config"] = dapui.close
             dapui.setup()
         end,
-        keys = {
-            { "<leader>dt", function() require("dap").toggle_breakpoint() end, desc = "DAP Toggle Breakpoint" },
-            { "<leader>db", function() require("dap").set_breakpoint(vim.fn.input("Condition: ")) end, desc = "DAP Conditional Breakpoint" },
-            { "<leader>dp", function() require("dap").set_breakpoint(nil, vim.fn.input("Log point message: ")) end, desc = "DAP Log Point" },
-            { "<leader>de", function() require("dap").set_exception_breakpoints({ "all" }) end, desc = "DAP Exception Breakpoints" },
-            { "<leader>dc", function() require("dap").continue() end, desc = "DAP Continue" },
-            { "<leader>di", function() require("dap").step_into() end, desc = "DAP Step Into" },
-            { "<leader>do", function() require("dap").step_over() end, desc = "DAP Step Over" },
-            { "<leader>du", function() require("dap").step_out() end, desc = "DAP Step Out" },
-            { "<leader>dl", function() require("dap").run_last() end, desc = "DAP Run Last" },
-            { "<leader>dr", function() require("dap").repl.open() end, desc = "DAP REPL Open" },
-            { "<leader>dd", function() require("dap").disconnect() end, desc = "DAP Disconnect" },
-        },
+        keys = function(_, keys)
+            local dap = require("dap")
+
+            return {
+                { "<leader>dt", function() dap.toggle_breakpoint() end, desc = "DAP Toggle Breakpoint" },
+                { "<leader>db", function() dap.set_breakpoint(vim.fn.input("Condition: ")) end, desc = "DAP Conditional Breakpoint" },
+                { "<leader>dp", function() dap.set_breakpoint(nil, vim.fn.input("Log point message: ")) end, desc = "DAP Log Point" },
+                { "<leader>de", function() dap.set_exception_breakpoints({ "all" }) end, desc = "DAP Exception Breakpoints" },
+                { "<leader>dc", function() dap.continue() end, desc = "DAP Continue" },
+                { "<leader>di", function() dap.step_into() end, desc = "DAP Step Into" },
+                { "<leader>do", function() dap.step_over() end, desc = "DAP Step Over" },
+                { "<leader>du", function() dap.step_out() end, desc = "DAP Step Out" },
+                { "<leader>dl", function() dap.run_last() end, desc = "DAP Run Last" },
+                { "<leader>dr", function() dap.repl.open() end, desc = "DAP REPL Open" },
+                { "<leader>dd", function() dap.disconnect() end, desc = "DAP Disconnect" },
+                unpack(keys),
+            }
+        end,
     },
 }
