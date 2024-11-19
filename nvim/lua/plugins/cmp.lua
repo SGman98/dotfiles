9afd59a3370dd4695e5e3a5cb8f33add8962165e
SGman98 <bastiangarnica@gmail.com>
2024-11-19 15:41:43 -0500

    refactor(nvim): ♻️ update cmp dependencies management

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,15 +6,13 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
-            "rafamadriz/friendly-snippets",
             "saadparwaiz1/cmp_luasnip",
             {
                 "L3MON4D3/LuaSnip",
-                config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load()
-
-                    require("luasnip").filetype_extend("typescriptreact", { "javascriptreact" })
-                end,
+                config = function() require("luasnip").filetype_extend("typescriptreact", { "javascriptreact" }) end,
+                dependencies = {
+                    { "rafamadriz/friendly-snippets", config = function() require("luasnip.loaders.from_vscode").lazy_load() end },
+                },
             },
             { "petertriho/cmp-git", opts = {} },
             {
