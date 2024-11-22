c24cf9c26cb379eb35c5241ee1f17442f80fde16
SGman98 <bastiangarnica@gmail.com>
2024-11-21 22:20:14 -0500

    feat(nvim): 🚨 disable warning

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -3,6 +3,7 @@ return {
     event = "VeryLazy",
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     config = function()
+        ---@diagnostic disable-next-line: missing-fields
         require("Comment").setup({
             pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
         })
