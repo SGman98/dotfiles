52041617ef053723545df82a7475f100637432fe
SGman98 <bastiangarnica@gmail.com>
2024-11-28 15:54:32 -0500

    feat(nvim): ✨ update telescope mappings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,17 +10,31 @@ return {
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
         config = function()
-            require("telescope").setup({
+            local telescope = require("telescope")
+            local lga_actions = require("telescope-live-grep-args.actions")
+            telescope.setup({
                 pickers = {
                     colorscheme = {
                         enable_preview = true,
                     },
                 },
+                extensions = {
+                    live_grep_args = {
+                        auto_quoting = true,
+                        mappings = {
+                            i = {
+                                ["<C-k>"] = lga_actions.quote_prompt(),
+                                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                                ["<C-space>"] = lga_actions.to_fuzzy_refine,
+                            },
+                        },
+                    },
+                },
             })
 
             -- Load extensions
-            require("telescope").load_extension("fzf")
-            require("telescope").load_extension("live_grep_args")
+            telescope.load_extension("fzf")
+            telescope.load_extension("live_grep_args")
         end,
         cmd = "Telescope",
         keys = {
@@ -46,7 +60,7 @@ return {
                 end,
                 desc = "Search Git Files",
             },
-            { "<leader>sc", function() require("telescope.builtin").grep_string() end, desc = "Search current word" },
+            { "<leader>sc", function() require("telescope-live-grep-args.shortcuts").grep_word_under_cursor() end, desc = "Search current word" },
             { "<leader>sw", function() require("telescope").extensions.live_grep_args.live_grep_args() end, desc = "Search by grep" },
             -- VIM PICKERS
             { "<leader>sb", function() require("telescope.builtin").buffers() end, desc = "Search buffers" },
