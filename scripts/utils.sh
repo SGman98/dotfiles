12593e8bb6e6db14909addebeb4a30cce5533020
SGman98 <bastiangarnica@gmail.com>
2025-05-09 09:04:40 -0500

    feat(bootstrap): add gpg and ssh util functions

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -35,3 +35,45 @@ utils::ansi_grid() {
 		printf "\n"
 	done
 }
+
+utils::config_gpg() {
+	if pkg::install "gpg"; then
+		log::info "Creating gpg key"
+		select opt in "New" "Existing"; do
+			case $opt in
+			"New")
+				local gpg_id
+				gpg --full-generate-key
+				gpg --list-secret-keys --keyid-format LONG
+				gpg_id=$(gpg --list-secret-keys --keyid-format LONG | grep sec | cut -d "/" -f 2 | cut -d " " -f 1)
+				important "Please copy this key and add it to github at https://github.com/settings/keys"
+				gpg --armor --export "$gpg_id"
+				break
+				;;
+			"Existing")
+				local private_path public_path
+				log::ask -ep "Enter path to gpg private key (leave empty to skip): " private_path
+				[ -n "$private_path" ] && gpg --import "$private_path"
+				log::ask -ep "Enter path to gpg public key (leave empty to skip): " public_path
+				[ -n "$public_path" ] && gpg --import "$public_path"
+				break
+				;;
+			*) log::abort "Invalid option" ;;
+			esac
+		done
+	fi
+}
+
+utils::config_ssh() {
+	if pkg::install "ssh"; then
+		local email
+		log::info "Creating ssh key"
+		log::ask -p "Enter your email for ssh key: " email
+		ssh-keygen -t ed25519 -C "$email"
+		eval "$(ssh-agent -s)"
+		ssh-add ~/.ssh/id_ed25519
+		log::info "ssh key created"
+		log::important "Copy this key and add it to github at https://github.com/settings/keys"
+		cat ~/.ssh/id_ed25519.pub
+	fi
+}
