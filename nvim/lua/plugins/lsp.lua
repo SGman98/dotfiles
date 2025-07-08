585ad73f0ac0a40dd6749b03945edf544d08ab07
SGman98 <bastiangarnica@gmail.com>
2025-07-07 22:25:25 -0500

    feat(nvim): ✨ update deprecated diagnostic sign definition

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -105,15 +105,15 @@ return {
         lspconfig.uiua.setup({})
 
         vim.diagnostic.config({
-            -- virtual_text = { source = true },
             virtual_lines = { current_line = true },
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+            },
         })
-
-        -- Signs
-        local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
     end,
 }
